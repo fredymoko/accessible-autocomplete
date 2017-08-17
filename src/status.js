5bf586ee5cc319eafb5b6347d876ef9c8fbb6465
Caroline Green <caroline.green@digital.cabinet-office.gov.uk>
2017-08-17 17:21:15 +0100

    WIP - Fixed comma issue.  This is going further into a hack but does work for voicover - need to test for JAWS and NVDA.

--- src/status.js
+++ src/status.js
@@ -73,7 +73,7 @@ export default class Status extends Component {
       }}
     >
       {content}
-      <span>{bump ? ',' : ',,'}</span>
+      <span aria-hidden='true'>{bump ? ',' : ',,'}</span>
     </div>
   }
 }
